9154a9b9380dab7159feffaa4f48a82489db91a4
root <root@srv1355666.hstgr.cloud>
2026-02-23 19:45:16 +0000

    feat: S01 - Project scaffolding and base configuration

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AKpGmT630RX1iulCWV7Plys0qP6r1zTUpd9qPxfa2ks=\"\n}"
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-xl">Welcome to Expense Tracker</h1>
+    </div>
+  );
+};
+
+export default Page;
